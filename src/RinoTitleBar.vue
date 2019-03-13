45cdd0a2c064024ab7863fb3966e1aa078501dea
Emmanuel Oppong-Sarpong <esarpong51@gmail.com>
2019-03-13 05:52:26 +0000

    Initial scaffold deleted

---
+++ src/RinoTitleBar.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="titlebar">
+    <div class="logo">
+      <img src="Fonts.png" alt>
+    </div>
+    <div class="content">
+      <div class="app-name">RINO Fonts</div>
+      <div id="center"></div>
+      <div id="right"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: [
+        
+    ]
+};
+</script>
+
+<style lang="scss">
+#titlebar {
+  position: relative;
+  height: 50px;
+  width: 100%;
+  box-shadow: 0 3px 4px rgba(0, 0, 0, 0.35);
+  z-index: 100;
+  background-color: white;
+  & > div {
+    display: inline-block;
+    height: 100%;
+  }
+  & > .content {
+    display: inline-flex;
+    height: 100%;
+    width: calc(100% - 50px);
+    align-items: center;
+    padding-left: 10px;
+  }
+
+  .app-name {
+    font-family: "Podkova VF Beta";
+    font-size: 20px;
+  }
+
+  & > .logo {
+    width: 50px;
+    img {
+      width: 100%;
+      height: auto;
+    }
+  }
+}
+</style>
